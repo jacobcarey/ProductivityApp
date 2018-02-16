2fc347f403788cd066dcaa1645bbb465f1136778
Jacob <jacobcarey95@googlemail.com>
2018-02-16 17:59:19 +0000

    Progress bar. JC.

--- app/src/main/java/com/example/b5043566/csc3122/BuildingActivity.java
+++ app/src/main/java/com/example/b5043566/csc3122/BuildingActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 // Make main activity.
@@ -16,32 +17,51 @@ public class BuildingActivity extends AppCompatActivity {
     private Runnable runnable;
     TextView coins;
     ImageView bolt;
+    ProgressBar progressBar;
+    Button powerUp;
+    boolean studying = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_building);
 
-        final Button powerUp = (Button) findViewById(R.id.powerUp);
+        powerUp = (Button) findViewById(R.id.powerUp);
         coins = (TextView) findViewById(R.id.coins);
         bolt = (ImageView) findViewById(R.id.bolt);
+        progressBar = (ProgressBar)findViewById(R.id.progressBar);
 
         bolt.setVisibility(View.GONE);
         powerUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                bolt.setVisibility(View.VISIBLE);
+                if(!studying){
+                    studying = true;
+                    powerUp.setText("Stop");
+                    bolt.setVisibility(View.VISIBLE);
 
-                final int time = 2000;
-                handler = new Handler();
+                    final int time = 2000;
+                    handler = new Handler();
 
-                handler.postDelayed(runnable = new Runnable() {
-                    public void run() {
-                        coins.setText(String.valueOf(i++));
-                        handler.postDelayed(this, time);
-                    }
-                }, time);
+                    handler.postDelayed(runnable = new Runnable() {
+                        public void run() {
+                            coins.setText(String.valueOf(i++));
+                            progressBar.setProgress(i);
+                            handler.postDelayed(this, time);
+                        }
+                    }, time);
+                }else{
+                    studying = false;
+                    bolt.setVisibility(View.GONE);
+                    powerUp.setText("Study");
+                    handler.removeCallbacks(runnable);
+                    coins.setText(String.valueOf(0));
+                    i = 0;
+                }
+
+
+
 
             }
         });
