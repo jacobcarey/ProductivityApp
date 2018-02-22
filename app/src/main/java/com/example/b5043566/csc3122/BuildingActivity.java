4a08d97389072454ba4f8886b925c6eb85432844
Jacob <jacobcarey95@googlemail.com>
2018-02-22 22:35:22 +0000

    Added hamburger menu (ish) and fixed layouts. JC.

--- app/src/main/java/com/example/b5043566/csc3122/BuildingActivity.java
+++ app/src/main/java/com/example/b5043566/csc3122/BuildingActivity.java
@@ -1,13 +1,19 @@
 package com.example.b5043566.csc3122;
 
 import android.os.Handler;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+
 
 // Make main activity.
 public class BuildingActivity extends AppCompatActivity {
@@ -15,10 +21,11 @@ public class BuildingActivity extends AppCompatActivity {
     private int i = 0;
     private Handler handler;
     private Runnable runnable;
-    TextView coins;
-    ImageView bolt;
-    ProgressBar progressBar;
-    Button powerUp;
+    private TextView coins;
+    private ImageView bolt;
+    private ProgressBar progressBar;
+    private Button powerUp;
+    private DrawerLayout mDrawerLayout;
     boolean studying = false;
 
     @Override
@@ -26,10 +33,35 @@ public class BuildingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_building);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+//        ActionBar actionbar = getSupportActionBar();
+//        actionbar.setDisplayHomeAsUpEnabled(true);
+//        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
         powerUp = (Button) findViewById(R.id.powerUp);
         coins = (TextView) findViewById(R.id.coins);
         bolt = (ImageView) findViewById(R.id.bolt);
         progressBar = (ProgressBar)findViewById(R.id.progressBar);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        mDrawerLayout.closeDrawers();
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+
+                        return true;
+                    }
+                });
 
         bolt.setVisibility(View.GONE);
         powerUp.setOnClickListener(new View.OnClickListener() {
@@ -65,6 +97,16 @@ public class BuildingActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 
---
+++ app/src/main/java/com/example/b5043566/csc3122/User.java
@@ -0,0 +1,12 @@
+package com.example.b5043566.csc3122;
+
+/**
+ * Created by b5043566 on 22/02/2018.
+ */
+
+public class User {
+
+    private String name;
+    private int power;
+
+}
